16553e29527f62bb80bc3da0e739528e2b1e6c74
Peter Gribanov <wcode404@gmail.com>
2016-06-23 16:00:38 +0300

    set schedule for full day in simple schedule functional test

--- tests/functional/simple_schedule.php
+++ tests/functional/simple_schedule.php
@@ -14,8 +14,11 @@ require __DIR__.'/../bootstrap.php';
 echo 'Simple schedule functional test.'.PHP_EOL;
 
 $smart = new SmartSleep(new Schedule([
-    (new EverydayRule())->setStart(9)->setEnd(15)->setSeconds(100), // [9:00, 15:00)
-    (new EverydayRule())->setStart(17)->setEnd(21)->setSeconds(60), // [17:00, 21:00)
+    (new EverydayRule())->setStart(0)->setEnd(3)->setSeconds(260), // [00:00, 03:00)
+    (new EverydayRule())->setStart(3)->setEnd(9)->setSeconds(900), // [03:00, 09:00)
+    (new EverydayRule())->setStart(9)->setEnd(19)->setSeconds(160), // [09:00, 19:00)
+    (new EverydayRule())->setStart(19)->setEnd(23)->setSeconds(70), // [19:00, 23:00)
+    (new EverydayRule())->setStart(23)->setEnd(24)->setSeconds(60), // [23:00, 24:00)
 ]));
 
 $seconds = $smart->getSleepSeconds(new \DateTime());
