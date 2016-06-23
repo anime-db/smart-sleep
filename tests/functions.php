79cfd98f1e8d019a2d0c9419653850ff0e091091
Peter Gribanov <wcode404@gmail.com>
2016-06-23 14:32:07 +0300

    fix error: Division by zero

--- tests/functions.php
+++ tests/functions.php
@@ -11,10 +11,10 @@
 function formatSeconds($seconds)
 {
     $hour = floor($seconds / 3600);
-    $seconds = $seconds % ($hour * 3600);
+    $seconds = $hour ? $seconds % ($hour * 3600) : $seconds;
 
     $minute = floor($seconds / 60);
-    $seconds = $seconds % ($minute * 60);
+    $seconds = $minute ? $seconds % ($minute * 60) : $seconds;
 
     return sprintf('%s:%s:%s', $hour, $minute, $seconds);
 }
