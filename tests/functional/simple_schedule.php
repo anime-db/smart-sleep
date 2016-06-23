6dc9dc291d094654c0facdb94ee362720082b084
Peter Gribanov <wcode404@gmail.com>
2016-06-23 15:40:02 +0300

    format code

--- tests/functional/simple_schedule.php
+++ tests/functional/simple_schedule.php
@@ -14,8 +14,8 @@ require __DIR__.'/../bootstrap.php';
 echo 'Simple schedule functional test.'.PHP_EOL;
 
 $smart = new SmartSleep(new Schedule([
-    (new EverydayRule())->setStart(9)->setEnd(15)->setSeconds(100), // [9:00, 15:00)
-    (new EverydayRule())->setStart(17)->setEnd(21)->setSeconds(60), // [17:00, 21:00)
+    (new EverydayRule())->setStart( 9)->setEnd(15)->setSeconds(100), // [ 9:00, 15:00)
+    (new EverydayRule())->setStart(17)->setEnd(21)->setSeconds( 60), // [17:00, 21:00)
 ]));
 
 $seconds = $smart->getSleepSeconds(new \DateTime());
