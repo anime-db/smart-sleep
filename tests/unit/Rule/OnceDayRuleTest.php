20ae19d8b1532ab1f213f4995335bebf44b89b59
Peter Gribanov <wcode404@gmail.com>
2016-06-23 13:48:42 +0300

    test OnceDayRule

---
+++ tests/unit/Rule/OnceDayRuleTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * AnimeDb package.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ */
+namespace AnimeDb\SmartSleep\Tests\Unit\Rule;
+
+use AnimeDb\SmartSleep\Rule\OnceDayRule;
+
+class OnceDayRuleTest extends TestCase
+{
+    /**
+     * @var OnceDayRule
+     */
+    protected $rule;
+
+    protected function setUp()
+    {
+        $this->rule = new OnceDayRule();
+    }
+
+    /**
+     * @return OnceDayRule
+     */
+    protected function getRule()
+    {
+        return $this->rule;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGettersAndSetters()
+    {
+        return [
+            ['getStart', 'setStart', -1],
+            ['getEnd', 'setEnd', -1],
+            // not check get/set seconds
+        ];
+    }
+
+    public function testSetSeconds()
+    {
+        // setted seconds not used
+        $this->assertEquals($this->rule, $this->rule->setSeconds(123));
+    }
+
+    public function testGetSecondsFromConstruct()
+    {
+        $offset = strtotime('+1 day 00:00:00') - time();
+
+        $seconds = $this->rule->getSeconds();
+
+        // -1 seconds because is long wait execute test
+        $this->assertTrue($seconds >= -1 && $seconds < $offset + 86400);
+    }
+
+    public function testGetSecondsFromMatched()
+    {
+        $time = new \DateTime('23-06-2016 13:42:15');
+        $offset = ((24 - 14) * 3600) + ((60 - 43) * 60) + (60 - 15);
+
+        $this->rule->isMatched($time);
+
+        $seconds = $this->rule->getSeconds();
+        $this->assertTrue($seconds >= 0 && $seconds < $offset + 86400);
+    }
+}
