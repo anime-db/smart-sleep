a423d37875de5e4bd6051f6f216d8da2648fa2c1
Peter Gribanov <wcode404@gmail.com>
2017-07-25 11:43:59 +0300

    RandMaxSecondsRuleBase use 0 as a min sleep seconds

--- src/Rule/RandMaxSecondsRuleBase.php
+++ src/Rule/RandMaxSecondsRuleBase.php
@@ -11,30 +11,10 @@ namespace AnimeDb\SmartSleep\Rule;
 abstract class RandMaxSecondsRuleBase extends RuleBase
 {
     /**
-     * Default sleep at least seconds.
-     *
-     * @var int
-     */
-    const DEFAULT_MIN_SLEEP_SECONDS = 10;
-
-    /**
-     * @var int
-     */
-    protected $min_sleep_seconds;
-
-    /**
-     * @param int $min_sleep_seconds
-     */
-    public function __construct($min_sleep_seconds = self::DEFAULT_MIN_SLEEP_SECONDS)
-    {
-        $this->min_sleep_seconds = $min_sleep_seconds;
-    }
-
-    /**
      * @return int
      */
     public function seconds()
     {
-        return rand($this->min_sleep_seconds, parent::seconds());
+        return rand(0, parent::seconds());
     }
 }
